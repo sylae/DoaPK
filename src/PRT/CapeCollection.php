6e05f57e28eeed432fdf7a29b5f242924c880e5e
Keira Aro <sylae@calref.net>
2018-11-12 06:37:04 -0800

    make loading static so i can call this wherever i please

--- src/PRT/CapeCollection.php
+++ src/PRT/CapeCollection.php
@@ -20,34 +20,36 @@ class CapeCollection extends \CharlotteDunois\Collect\Collection
 
     function __construct(mixed $data = null)
     {
-        $capes = [];
-        foreach (glob("data/Capes/*.json") as $file) {
-            foreach (json_decode(file_get_contents($file), true) as $x) {
-                $c = new Cape();
-                foreach ($x as $arg => $value) {
-                    if (is_null($arg)) {
-                        continue;
-                    }
-                    switch ($arg) {
-                        case "PIR":
-                            $c->mainPIR = new PIR(hexdec($value[0]), new Carbon($value[1]), $value[2]);
-                            break;
-                        case "civ":
-                            if (!is_array($value)) {
+        static $capes = [];
+        if (count($capes) == 0) {
+            foreach (glob("data/Capes/*.json") as $file) {
+                foreach (json_decode(file_get_contents($file), true) as $x) {
+                    $c = new Cape();
+                    foreach ($x as $arg => $value) {
+                        if (is_null($arg)) {
+                            continue;
+                        }
+                        switch ($arg) {
+                            case "PIR":
+                                $c->mainPIR = new PIR(hexdec($value[0]), new Carbon($value[1]), $value[2]);
                                 break;
-                            }
-                            foreach ($value as $civArg => $civValue) {
-                                $c->civID->$civArg = $civValue;
-                            }
-                            if (is_string($c->civID->dob)) {
-                                $c->civID->dob = new Carbon($c->civID->dob);
-                            }
-                            break;
-                        default:
-                            $c->$arg = $value;
+                            case "civ":
+                                if (!is_array($value)) {
+                                    break;
+                                }
+                                foreach ($value as $civArg => $civValue) {
+                                    $c->civID->$civArg = $civValue;
+                                }
+                                if (is_string($c->civID->dob)) {
+                                    $c->civID->dob = new Carbon($c->civID->dob);
+                                }
+                                break;
+                            default:
+                                $c->$arg = $value;
+                        }
                     }
+                    $capes[$c->name] = $c;
                 }
-                $capes[$c->name] = $c;
             }
         }
         parent::__construct($capes);
