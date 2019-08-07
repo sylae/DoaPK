b4363b9c4bc82854335ce75a8a44dc25d29955e6
Keira Dueck <sylae@calref.net>
2019-08-07 06:29:21 -0700

    move data loading to a bootstrap

--- index.php
+++ index.php
@@ -19,73 +19,7 @@ use Twig\TwigTest;
 use function FastRoute\simpleDispatcher;
 
 require_once "vendor/autoload.php";
-
-$nowDate = Carbon::now("America/Los_Angeles")->setTimezone("America/Los_Angeles")->setDate(2011, 7, 14);
-Carbon::setTestNow($nowDate);
-
-// store our PIR links because we can't connect them until this is done :v
-$pirs = [];
-
-// DATA LOADING STEP ONE (CAPES)
-foreach (glob("data/Capes/*.json") as $file) {
-    foreach (json_decode(file_get_contents($file), true) as $x) {
-        $c = new Cape((int) hexdec($x['PIR'][0]), new Carbon($x['PIR'][1]), $x['PIR'][2]);
-        foreach ($x as $arg => $value) {
-            if (is_null($arg)) {
-                continue;
-            }
-            switch ($arg) {
-                case "PIR":
-                    break;
-                case "links":
-                    $pirs[$c->getTag()] = $value;
-                    break;
-                case "civ":
-                    if (!is_array($value)) {
-                        break;
-                    }
-                    foreach ($value as $civArg => $civValue) {
-                        $c->civID->$civArg = $civValue;
-                    }
-                    if (is_string($c->civID->dob)) {
-                        $c->civID->dob = new Carbon($c->civID->dob);
-                    }
-                    break;
-                default:
-                    $c->$arg = $value;
-            }
-        }
-    }
-}
-
-// DATA LOADING STEP TWO (ART)
-foreach (glob("data/Art/*.json") as $file) {
-    foreach (json_decode(file_get_contents($file), true) as $x) {
-        $c = new Media((int) hexdec($x['PIR'][0]), new Carbon($x['PIR'][1]), $x['PIR'][2]);
-        foreach ($x as $arg => $value) {
-            if (is_null($arg)) {
-                continue;
-            }
-            switch ($arg) {
-                case "PIR":
-                    break;
-                case "links":
-                    $pirs[$c->getTag()] = $value;
-                    break;
-                default:
-                    $c->$arg = $value;
-            }
-        }
-    }
-}
-
-// CONNECT PIRS
-foreach ($pirs as $source => $array) {
-    $source = PIR::pirDB()->get($source);
-    foreach ($array as $dest) {
-        $source->addRef(PIR::pirDB()->get($dest));
-    }
-}
+require_once "bootstrap.php";
 
 
 // THE ACTUAL ROUTER
